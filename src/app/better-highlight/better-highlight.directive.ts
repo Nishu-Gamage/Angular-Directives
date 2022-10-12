4cd0a5632210dbb2a8d6632e81c117c63db03350
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-10-12 11:48:58 +0900

    Use  「 @ HostBinding 」- Righ t way to code directive - Part 3

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,10 +1,12 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective {
 
+  @HostBinding("style.backgroundColor") backgroundcolor:string = "transparent";
+  
   constructor(private renderer:Renderer2, private elementRef:ElementRef) { }
 
   ngOnInit(): void {
@@ -13,11 +15,13 @@ export class BetterHighlightDirective {
   }
 
   @HostListener("mouseenter") mouseOver(eventdata:Event){
-    this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue")
+    // this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue")
+    this.backgroundcolor="blue";
   }
 
   @HostListener("mouseleave") mouseLeave(eventdata:Event){
-    this.renderer.setStyle(this.elementRef.nativeElement,"background-color","transparent")
+    // this.renderer.setStyle(this.elementRef.nativeElement,"background-color","transparent")
+    this.backgroundcolor="pink";
   }
-  
+
 }
