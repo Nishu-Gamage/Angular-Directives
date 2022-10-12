819eeabbeb71ad8ec84e933e4e5fc68bbab91e7f
Nishu <67893576+NishuGamage@users.noreply.github.com>
2022-10-12 11:29:04 +0900

    Use  「 @ HostListener 」- Righ t way to code directive - Part 2

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -12,4 +12,12 @@ export class BetterHighlightDirective {
     this.renderer.setStyle(this.elementRef.nativeElement,"padding",".5em")
   }
 
+  @HostListener("mouseenter") mouseOver(eventdata:Event){
+    this.renderer.setStyle(this.elementRef.nativeElement,"background-color","blue")
+  }
+
+  @HostListener("mouseleave") mouseLeave(eventdata:Event){
+    this.renderer.setStyle(this.elementRef.nativeElement,"background-color","transparent")
+  }
+  
 }
